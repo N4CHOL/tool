3f6a7d059aa6e3e28dccc47202e3634b115fc657
N4CHOL <leuca.ignacio@gmail.com>
2025-01-08 15:20:19 -0300

    c

--- README.md
+++
@@ -1,1 +0,0 @@
-# Portongdrive
--- files/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ListFilesView, UploadFileView, DownloadFileView
-
-urlpatterns = [
-    path('list-files/', ListFilesView.as_view(), name='list-files'),
-    path('upload-file/', UploadFileView.as_view(), name='upload-file'),
-    path('download-file/', DownloadFileView.as_view(), name='upload-file'),
-]
