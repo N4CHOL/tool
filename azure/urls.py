1b7f0f6c562adba58082acac7d11ab1e469b6ae7
N4CHOL <leuca.ignacio@gmail.com>
2025-01-16 17:15:01 -0300

    d

--- azure/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import process_and_ocr  # Import the view
-
-urlpatterns = [
-    path('ocr/', process_and_ocr, name='ocr'),  # Keep the route as /ocr/
-]
--- files/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class File(models.Model):
-    name = models.CharField(max_length=255)
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    file_path = models.FilePathField(path="/uploads")
-
-    def __str__(self):
-        return self.name
